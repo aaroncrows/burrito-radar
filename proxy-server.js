a45deec0791bee1c33dd46f3554b5c7ca6b352d3
Aaron Krause <aaronjkrause@gmail.com>
2017-12-21 22:39:18 -0800

    update to use api key

--- proxy-server.js
+++ proxy-server.js
@@ -1,38 +1,28 @@
-require('./scripts/keys')
-
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const request = require('request')
 const cors = require('cors')
-let token
-const { YELP_CLIENT_SECRET: secret, YELP_CLIENT_ID: id } = process.env
+
+// load keys locally for development
+require('./scripts/keys')
 
 app.use(cors())
 
 app.post('/graphql', bodyParser.json(), (req, res) => {
-  request.post('https://api.yelp.com/oauth2/token', {
-      form: {
-        client_id: id,
-        client_secret: secret,
-        grant_type: 'client_credentials'
-      }
-  }, (err, r, b) => {
-    if (err) return res.json(err)
-    b = JSON.parse(b)
-    const token = b.access_token
-    request.post('https://api.yelp.com/v3/graphql', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(req.body)
-    }, (err, r) => {
-      if (err) return res.json(err)
-      r = JSON.parse(r.body)
+  const token = process.env.API_KEY
 
-      res.json(r)
-    })
+  request.post('https://api.yelp.com/v3/graphql', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(req.body)
+  }, (err, r) => {
+    if (err) return res.json(err)
+    r = JSON.parse(r.body)
+
+    res.json(r)
   })
 })
 
